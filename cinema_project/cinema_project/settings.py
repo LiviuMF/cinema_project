876376407e791db0342ef41e44fd310515bc5ede
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-07-08 10:25:58 +0300

    DB setup

--- cinema_project/cinema_project/settings.py
+++ cinema_project/cinema_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cinema_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cinema_project',
+        'USER': 'postgres',
+        'PASSWORD': 'test123123',
+        'HOST': 'localhost',
+        'PORT': '8001',
     }
 }
 
