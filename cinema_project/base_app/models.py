6ee9fcbd002ede1215221593e95fae558f25278a
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-07-11 09:44:07 +0300

    commit model and migraitons

--- cinema_project/base_app/models.py
+++ cinema_project/base_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=200, default='')
+    name = models.CharField(max_length=200, default=None)
     poster = models.ImageField(default=None)
-    description = models.TextField(default='')
+    description = models.TextField(default=None)
     year = models.FloatField(default=None)
     director = models.CharField(max_length=200, default=None)
     imdb_link = models.CharField(max_length=400, default='https://imdb.com')
     imdb_id = models.FloatField(default=None)
-    url = models.CharField(max_length=400, default='')
+    url = models.CharField(max_length=400, default=None)
     duration = models.FloatField(default=None)
 
     def __str__(self):
