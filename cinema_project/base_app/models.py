be212ffa23fd4aaf454b238ebc1539b52e1e3303
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-07-11 10:07:08 +0300

    add model movie hall

--- cinema_project/base_app/models.py
+++ cinema_project/base_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Hall(models.Model):
+    name = models.CharField(max_length=200, default=None)
+    description = models.TextField(max_length=400, default=None)
+    seats = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     name = models.CharField(max_length=200, default=None)
     poster = models.ImageField(default=None)
@@ -11,6 +20,7 @@ class Movie(models.Model):
     imdb_id = models.FloatField(default=None)
     url = models.CharField(max_length=400, default=None)
     duration = models.FloatField(default=None)
+    hall = models.ForeignKey(Hall, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
