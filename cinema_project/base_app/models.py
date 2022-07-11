c853a1f972b29e57d8cc0935ef87a99b321a0aca
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-07-11 09:15:00 +0300

    create am register Movie model

--- cinema_project/base_app/models.py
+++ cinema_project/base_app/models.py
@@ -5,7 +5,12 @@ class Movie(models.Model):
     name = models.CharField(max_length=200)
     poster = models.ImageField()
     description = models.TextField()
-    imdb = models.FloatField()
+    year = models.FloatField()
+    director = models.CharField(200)
+    imdb_link = models.CharField(max_length=400)
+    imdb_id = models.FloatField()
     url = models.CharField(max_length=400)
     duration = models.FloatField()
-    
+
+    def __str__(self):
+        return self.name
