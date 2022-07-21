4aa3769c63f4291e37a0f33030b046192b38395c
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-07-21 11:42:51 +0300

    register reservation model to admin

--- cinema_project/base_app/admin.py
+++ cinema_project/base_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Movie, ContactMessages, Hall, Schedule
+from .models import Movie, ContactMessages, Hall, Schedule, Reservation
 
 admin.site.register(Movie)
 admin.site.register(ContactMessages)
 admin.site.register(Hall)
 admin.site.register(Schedule)
+admin.site.register(Reservation)
