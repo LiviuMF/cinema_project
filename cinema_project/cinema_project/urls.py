686bd9219c7d436af67e7acae580aded2ba7ae49
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-08-02 10:07:44 +0300

    remove media files and add media folder

--- cinema_project/cinema_project/urls.py
+++ cinema_project/cinema_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('cinema/', include('base_app.urls')),
     path('members/', include('login.urls')),
     path('members/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
