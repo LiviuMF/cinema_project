972f0730c1d6c409bcb3dc8eedc6a226a02afae8
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-08-10 08:40:37 +0300

    Fix url regex in email conf / endline css

--- cinema_project/login/urls.py
+++ cinema_project/login/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('login/', views.login_user, name='login_page'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.signup, name='register_page'),
-    path(r"activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",
-        views.activate, name='activate'),
+    path("activate/<uidb64>/<token>/", views.activate, name='activate'),
 ]
