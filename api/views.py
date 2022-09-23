fdfab279c87fbe5af1e6b184013766b800ea989d
Liviu-Marius FARCAS <lifarcas@pentalog.com>
2022-09-23 11:54:06 +0300

    refactor code from func views to class views and other PR comments

--- api/views.py
+++ api/views.py
@@ -2,7 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework import filters, generics
 from rest_framework.response import Response
 
-from .serializers import MovieSerializer, MovieSerializerWithSchedules, HallSerializer, ScheduleSerializer, ReservationSerializer
+from .serializers import (
+    MovieSerializer,
+    MovieSerializerWithSchedules,
+    HallSerializer,
+    ScheduleSerializer,
+    ReservationSerializer
+)
 from cinema_project.utils import today, next_days
 from base_app.models import Movie, Cinema, Hall, Reservation, Schedule
 from . import permissions
@@ -22,87 +28,24 @@ def currently_playing_with_schedule(request):
     return Response(serializer.data)
 
 
-@api_view(['POST'])
-def create_hall(request):
-    cinema_name = request.data['cinema']
-    request.data['cinema'] = Cinema.objects.filter(name=cinema_name).first().pk
-    serializer = HallSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        print(f"Creating Hall Failed with following error: \n{serializer.errors}")
-    return Response(serializer.data)
+class HallListCreateUpdate(generics.CreateAPIView, generics.UpdateAPIView):
+    queryset = Hall.objects.all()
+    serializer_class = HallSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
 
 
-@api_view(['POST'])
-def update_hall(request):
-    hall_name = request.data['name']
-    cinema_name = request.data['cinema']
-    request.data['cinema'] = Cinema.objects.filter(name=cinema_name).first().pk
-    hall = Hall.objects.get(name=hall_name)
-    serializer = HallSerializer(instance=hall, data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def create_movie(request):
-    serializer = MovieSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        print(f"Creating Movie Failed with following error: \n{serializer.errors}")
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def update_movie(request):
-    movie_name = request.data['name']
-    movie_instance = Movie.objects.filter(name=movie_name).first()
-    serializer = MovieSerializer(instance=movie_instance, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def create_schedule(request):
-    serializer = ScheduleSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        print(f"Creating Schedule Failed with following error: \n{serializer.errors}")
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def update_schedule(request):
-    schedule = Schedule.objects.get(pk=request.data['schedule'])
-    serializer = ScheduleSerializer(instance=schedule, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        print(f"Updating Schedule Failed with following error: \n{serializer.errors}")
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def confirm_reservation(request):
-    reservation = Reservation.objects.get(pk=request.data['reservation'])
-    serializer = ReservationSerializer(instance=reservation, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        print(serializer.errors)
-    return Response(serializer.data)
-
-
-class MovieSearchView(generics.ListAPIView):
+class MovieListCreate(generics.ListCreateAPIView, generics.UpdateAPIView):
     search_fields = ['imdb_id', 'name']
     filter_backends = (filters.SearchFilter,)
     serializer_class = MovieSerializer
+
+    def get(self, request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = self.serializer_class(movies, many=True)
+        return Response(serializer.data)
 
     def get_queryset(self):
         queryset = Movie.objects.all()
@@ -114,8 +57,22 @@ class MovieSearchView(generics.ListAPIView):
             return queryset.filter(name=movie_name)
         return queryset
 
+    lookup_field = 'pk'
 
-class ReservationListCreate(generics.ListCreateAPIView):
+    def perform_update(self, serializer):
+        instance = serializer.save()
+
+
+class ScheduleListCreateUpdate(generics.CreateAPIView, generics.UpdateAPIView):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+
+
+class ReservationListCreateUpdate(generics.ListCreateAPIView, generics.UpdateAPIView):
     permission_classes = [permissions.IsOwner]
     serializer_class = ReservationSerializer
 
@@ -126,12 +83,7 @@ class ReservationListCreate(generics.ListCreateAPIView):
         else:
             return Reservation.objects.filter(user=self.request.user)
 
-
-class ReservationListUpdate(generics.UpdateAPIView):
-    queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
     lookup_field = 'pk'
-    permission_classes = [permissions.IsOwner]
 
     def perform_update(self, serializer):
         instance = serializer.save()
